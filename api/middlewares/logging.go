08678624f0c6451b04e51a728c7e27be29b02e03
engineer_tacky <tsubasa801@gmail.com>
2024-06-25 09:28:53 +0900

    ミドルウェア追加

---
+++ api/middlewares/logging.go
@@ -0,0 +1,29 @@
+package middlewares
+
+import (
+	"log"
+	"net/http"
+)
+
+type resLoggingWriter struct {
+	http.ResponseWriter
+	code int
+}
+
+func NewResLoggingWriter(w http.ResponseWriter) *resLoggingWriter {
+	return &resLoggingWriter{ResponseWriter: w, code: http.StatusOK}
+}
+
+func (rsw *resLoggingWriter) WriteHeader(code int) {
+	rsw.code = code
+	rsw.ResponseWriter.WriteHeader(code)
+}
+
+func LoggingMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, req *http.Request) {
+		log.Printf(req.RequestURI, req.Method)
+		rlw := NewResLoggingWriter(w)
+		next.ServeHTTP(w, req)
+		log.Println("res: ", rlw.code)
+	})
+}
--- api/router.go
+++ api/router.go
@@ -4,6 +4,7 @@ import (
 	"database/sql"
 	"net/http"
 
+	"github.com/engineer-tacky/go-blog-api/api/middlewares"
 	"github.com/engineer-tacky/go-blog-api/controllers"
 	"github.com/engineer-tacky/go-blog-api/services"
 	"github.com/gorilla/mux"
@@ -24,6 +25,8 @@ func NewRouter(db *sql.DB) *mux.Router {
 	r.HandleFunc("/article/nice", aCon.PostNiceHandler).Methods(http.MethodPost)
 
 	r.HandleFunc("/comment", cCon.PostCommentHandler).Methods(http.MethodPost)
+
+	r.Use(middlewares.LoggingMiddleware)
 
 	return r
 }
